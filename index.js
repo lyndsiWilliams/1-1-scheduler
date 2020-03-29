0b756c8920da7d687a2c3f8eea4460cc33295e26
Lyndsi Kay Williams <55605634+lyndsiWilliams@users.noreply.github.com>
2020-03-29 01:20:52 -0500

    Basic server is alive!

---
+++ index.js
@@ -0,0 +1,5 @@
+// Import server
+const server = require('./api/server.js');
+// Tell server where to listen
+const PORT = process.env.PORT || 6969;
+server.listen(PORT, () => console.log(`\n** Running on port: ${PORT} **\n`));
